2b9431ae7b6b87633ce515bd4647738c66fd0731
pranav2308 <pranav08@terpmail.umd.edu>
2019-08-20 23:56:41 -0400

    completing user registration

--- server.js
+++ server.js
@@ -80,19 +80,46 @@ app.get('/', (req, res) => {
 // 	})
 // })
 
+// addUserData = (trx) => {
+// 	return trx('login').insert({email : email, hash : hash})
+// 	.then(() => {
+// 		return trx('users').returning('*').insert({name : name, email : email, joindate : new Date()});
+// 	})
+// }
 
 app.post('/register', (req, res) => {
 	const { name, email, password } = req.body;
-	database('users').returning('*').insert({name : name, email : email, joindate : new Date()})
-	.then(user => res.status(200).json(user[0]))
-	.catch(error => {
-		if(parseInt(error.code) === 23505){
-			res.status(409).json("User with same email already exists. You can try signing-in or choose another email for registration");
-		}
-		else{
-			res.status(400).json(`Oops! Something went wrong. You have been struck with ${error}`);
-		}
+
+	bcrypt.genSalt(10, function(err, salt) {
+    	bcrypt.hash(password, salt, function(err, hash) {
+			database.transaction(function(trx){
+				return trx('login').insert({email : email, hash : hash})
+						.then(() => {
+							return trx('users').returning('*').insert({name : name, email : email, joindate : new Date()});
+						});
+			})
+			.then(user => res.status(200).json(user[0]))//respond to the front end with user data
+			.catch(error => {
+				if(parseInt(error.code) === 23505){
+					res.status(409).json("User with same email already exists. You can try signing-in or choose another email for registration");
+				}
+				else{
+					res.status(400).json(`Oops! Something went wrong. You have been struck with ${error}`);
+				}
+			});  
+    	});
 	});
+
+	// database('users').returning('*').insert({name : name, email : email, joindate : new Date()})
+	// .then(user => res.status(200).json(user[0]))
+	// .catch(error => {
+	// 	if(parseInt(error.code) === 23505){
+	// 		res.status(409).json("User with same email already exists. You can try signing-in or choose another email for registration");
+	// 	}
+	// 	else{
+	// 		res.status(400).json(`Oops! Something went wrong. You have been struck with ${error}`);
+	// 	}
+	// });
 });
 
 app.post('/signin', (req, res) => {
